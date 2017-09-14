c9e4b990ed85bb84e73e4d671c033406f3d24d38
Karsten Siebert <31937819+KarstenSiebert@users.noreply.github.com>
2017-09-14 08:00:28 +0200

    Added sample PHP scripts
    The files contain PHP scripts to send messages to the Ride message center. Local broadcast messages (caster.php), local geofencing enabled messages (poster.php), Push messages to an individual recipient (pusher.php). Messages will be pushed depending on type and usage.

---
+++ caster.php
@@ -0,0 +1,42 @@
+<?php
+
+		# Broadcast messages to a region (site) immediately
+		
+        $data = array (
+            'head' => 'Title of message', 
+            'text' => 'Short description of message', 
+            'link' => 'Link to site or element (image, video, other) (or empty)', # Appropriate app will be started to view / follow this link
+            'icon' => 'Link to icon, which is displayed with message (or empty)',
+            'site' => 'Address (street, city, or region, or known place), where to broadcast this message to',
+            'type' => 'See comment'
+        );
+		
+		# Comments
+		#
+		# type field is 1 or 2 or 4 or 8 or 16 depending on the category the message belongs to
+		#
+		# a user will only be notified and the message will be displayed, if the user has enabled the filter to receive messages of this category
+		#
+		# broadcast messages require additional approval from the backoffice site, contact me at info@siehog.com
+		#
+		
+        $headers = array (
+            'Authorization: key=app-key',		# Sender's key inside the app (app screen botton)
+            'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://push.barubox.com/messagesCast');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        curl_exec($ch);
+
+		curl_close($ch);
+ 
+?>
+
---
+++ pusher.php
@@ -0,0 +1,38 @@
+<?php
+
+		# Send messages to an individual recipient
+		
+	    $data = array (
+            'head' => 'Title of message', 
+            'text' => 'Short description of message', 
+            'link' => 'Link to site or element (image, video, other) (or empty)', # Appropriate app will be started to view / follow this link
+            'icon' => 'Link to icon, which is displayed with message (or empty)',
+        );     
+
+		# Comments
+		#
+		# No type field necessary, type is fixed: Private message center (16) 
+		#
+		# No site field, message will be deliverd independent of the location of the recipient
+		#
+		
+        $headers = array (
+		    'Authorization: key=app-key',		# Recipient's key inside the app (app screen botton)
+			'Content-Type: application/json'
+        );
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://push.barubox.com/messagesPush');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+
+        curl_exec($ch);
+
+		curl_close($ch);
+
+?>
+
