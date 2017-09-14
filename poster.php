5e5050a77c10f363b6dd5c65d4ac26b10c959626
Karsten Siebert <31937819+KarstenSiebert@users.noreply.github.com>
2017-09-14 08:07:30 +0200

    Update poster.php

--- poster.php
+++ poster.php
@@ -1,8 +1,8 @@
 <?php
 
-		# Send messages to a region (site) for later geofencing, users entering the site will receive these messages
+	# Send messages to a region (site) for later geofencing, users entering the site will receive these messages
 		
-	    $data = array (
+	$data = array (
             'head' => 'Title of message', 
             'text' => 'Short description of message', 
             'link' => 'Link to site or element (image, video, other) (or empty)', # Appropriate app will be started to view / follow this link
@@ -11,17 +11,17 @@
             'type' => 'See comment'
         );
 				
-		# Comments
-		#
-		# type field is 1 or 2 or 4 or 8 or 16 depending on the category the message belongs to
-		#
-		# a user will only be notified and the message will be displayed, if the user has enabled the filter to receive messages of this category
-		#
-		# geofencing messages require additional approval from the backoffice site, contact me at info@siehog.com
-		#
+	# Comments
+	#
+	# type field is 1 or 2 or 4 or 8 or 16 depending on the category the message belongs to
+	#
+	# a user will only be notified and the message will be displayed, if the user has enabled the filter to receive messages of this category
+	#
+	# geofencing messages require additional approval from the backoffice site, contact me at info@siehog.com
+	#
 		
         $headers = array (
-		    'Authorization: key=app-key',		# Sender's key inside the app (app screen botton)
+		    'Authorization: key=app-key',	# Sender's key inside the app (app screen botton)
 			'Content-Type: application/json'
         );
 
@@ -36,7 +36,6 @@
 
         curl_exec($ch);
 
-		curl_close($ch);
+	curl_close($ch);
 
 ?>
-
