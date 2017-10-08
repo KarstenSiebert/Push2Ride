20f48d89e456d4c057353fd19a0e433c8a090aab
Karsten Siebert <31937819+KarstenSiebert@users.noreply.github.com>
2017-10-08 13:47:43 +0200

    Update peer.php

--- peer.php
+++ peer.php
@@ -1,17 +1,16 @@
 <?php
 
         $data = array (
-            'head' => 'Nagios Message Center', 
-            'text' => 'SNMP Server went down', 
-            'link' => '',
-            'icon' => '',
-            'shot' => 'https://filecast.barubox.com/users/e7f889d688d74b3c5901ebad086d8477/407/34VDjfx4.jpg',  
-            'from' => 'Karsten',
-            'peer' => 'qwertzuiop'
+	    'head' => 'Title of message', 
+            'text' => 'Short description of message', 
+            'link' => 'Link to site or element (image, video, other) (or empty)', # Appropriate app will be started to view / follow this link
+            'icon' => 'Link to icon, which is displayed with message (or empty)',
+	    'shot' => 'Link to image file, which will be displayed with message (or empty)',
+	    'from' => 'From identifier (or empty)',
+            'peer' => 'Distribution group name'
         );
 
         $headers = array (
-#            'Authorization: key=e07b43f67b25858f',
             'Authorization: key=4c21c1c02e7e070',
             'Content-Type: application/json'
         );
@@ -26,13 +25,7 @@
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
         $result = curl_exec($ch);
-
-		if ($debug)
-		{
-        	$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        	file_put_contents('/tmp/client-peer', date('Y-m-d H:i').'-'.$httpcode.'-'.$result.PHP_EOL, FILE_APPEND);
-		}
-
+	
         curl_close($ch);
 
 ?>
