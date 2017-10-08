51701cded0ebbb01a378f47d9e78c4ebf7ef65d5
Karsten Siebert <31937819+KarstenSiebert@users.noreply.github.com>
2017-10-08 13:55:48 +0200

    Update peer.php

--- peer.php
+++ peer.php
@@ -6,12 +6,20 @@
             'link' => 'Link to site or element (image, video, other) (or empty)', # Appropriate app will be started to view / follow this link
             'icon' => 'Link to icon, which is displayed with message (or empty)',
 	    'shot' => 'Link to image file, which will be displayed with message (or empty)',
-	    'from' => 'From identifier (or empty)',
+	    'from' => 'From identifier', # A unique identifier of the sender
             'peer' => 'Distribution group name'
         );
 
+	
+	# Comments
+	#
+	# A user will only be notified and the message will be displayed, if the user has joined the peer group and enabled the option to receive private messages
+	#
+	# Inside the Ride app several groups can be joined by adding them separated with comma
+	#
+		
         $headers = array (
-            'Authorization: key=4c21c1c02e7e070',
+            'Authorization: key=app-key',	# Sender's key inside the Ride app
             'Content-Type: application/json'
         );
 
@@ -24,7 +32,7 @@
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
 
-        $result = curl_exec($ch);
+        curl_exec($ch);
 	
         curl_close($ch);
 
